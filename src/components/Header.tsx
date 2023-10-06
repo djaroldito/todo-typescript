77e7825a26adb5b107aed9047198c722990197b9
Ariel Monti <djaroldito@gmail.com>
2023-10-06 17:01:29 -0300

    5

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { TodoTitle } from "../types"
 import { CreateTodo } from "./CreateTodo"
+import './Header.css'
 
 interface Props {
     onAddTodo: ({title}: TodoTitle) => void
@@ -8,7 +9,7 @@ interface Props {
 export const Header:React.FC<Props> = ({onAddTodo}) => {
   return (
     <header className="header">
-        <h1>Arielito's to do 
+        <h1 style={{ fontSize: window.innerWidth <= 768 ? '46px' : '80px', top: window.innerWidth <= 768 ? '-90px' : '-140px' }}>Arielito's to do 
           {/* <img
         style={{width:'80px', height:'40px', borderRadius:'5px'}}
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcAzG-FilsJbdzLiNkl6FXrIoOMpr5c1lV-w&usqp=CAU"
